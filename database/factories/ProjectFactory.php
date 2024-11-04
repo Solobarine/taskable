69758ba62fc8a9a9f952e80f5f38d369b5f64035
Solly <solobarine@gmail.com>
2024-11-04 14:10:55 +0100

    projects: add factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(rand(2, 5)),
+            'description' => $this->faker->sentence(20, 40)
+        ];
+    }
+}
