5924f31a572394335645b9f6fbe9ee9a44d6ae9e
Solly <solobarine@gmail.com>
2024-11-04 14:14:39 +0100

    routes: add projects routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ProjectController::class)->group(function () {
+    Route::get('/projects', 'index')->name('projects');
+    Route::get('/projects/create', 'create')->name('projects.create');
+    Route::post('/projects', 'store')->name('projects.store');
+    Route::get('/projects/{project}', 'show')->name('projects.show');
+    Route::get('/projects/{project}/edit', 'edit')->name('projects.edit');
+    Route::patch('/projects/{project}', 'update')->name('projects.update');
+    Route::delete('/projects/{project}', 'destroy')->name('projects.destroy');
 });
